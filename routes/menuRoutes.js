554cf6ae67b8178bc1407d61d6a9bf1595603d3f
Marine4281 <marinepro360@gmail.com>
2026-07-18 20:48:47 +0300

    Add update and delete routes for menu items

--- routes/menuRoutes.js
+++ routes/menuRoutes.js
@@ -1,11 +1,18 @@
 // routes/menuRoutes.js
 import express from "express";
-import { getMenu, createMenuItem } from "../controllers/menuController.js";
+import {
+  getMenu,
+  createMenuItem,
+  updateMenuItem,
+  deleteMenuItem,
+} from "../controllers/menuController.js";
 import { protect, authorize } from "../Middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.get("/", getMenu);
 router.post("/", protect, authorize("admin", "manager", "waiter", "accountant"), createMenuItem);
+router.put("/:id", protect, authorize("admin", "manager", "waiter", "accountant"), updateMenuItem);
+router.delete("/:id", protect, authorize("admin", "manager", "waiter", "accountant"), deleteMenuItem);
 
 export default router;
