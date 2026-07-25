6ec3205aef5cefa4c7b55658f2c2a55126dec7d2
Marine4281 <marinepro360@gmail.com>
2026-07-25 13:08:55 +0300

    Add permission requirement for settings routes

--- routes/settingsRoutes.js
+++ routes/settingsRoutes.js
@@ -1,12 +1,12 @@
 // routes/settingsRoutes.js
 import express from "express";
-import { protect, authorize } from "../Middlewares/authMiddleware.js";
+import { protect, authorize, requirePermission } from "../Middlewares/authMiddleware.js";
 import { getSettings, updateSettings, getPublicSettings } from "../controllers/settingsController.js";
 
 const router = express.Router();
 
 router.get("/public", getPublicSettings);
-router.get("/", protect, authorize("admin"), getSettings);
-router.patch("/", protect, authorize("admin"), updateSettings);
+router.get("/", protect, authorize("admin", "accountant"), requirePermission("settings"), getSettings);
+router.patch("/", protect, authorize("admin", "accountant"), requirePermission("settings"), updateSettings);
 
 export default router;
