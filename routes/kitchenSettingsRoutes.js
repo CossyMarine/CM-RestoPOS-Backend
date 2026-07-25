6d7ba9312a260ce0d301dd183f9008cd634c5108
Marine4281 <marinepro360@gmail.com>
2026-07-25 13:09:44 +0300

    Add requirePermission to kitchen settings routes

--- routes/kitchenSettingsRoutes.js
+++ routes/kitchenSettingsRoutes.js
@@ -1,11 +1,11 @@
 // routes/kitchenSettingsRoutes.js
 import express from "express";
-import { protect, authorize } from "../Middlewares/authMiddleware.js";
+import { protect, authorize, requirePermission } from "../Middlewares/authMiddleware.js";
 import { getKitchenSettings, updateKitchenSettings } from "../controllers/kitchenSettingsController.js";
 
 const router = express.Router();
 
-router.get("/", protect, authorize("kitchen", "admin"), getKitchenSettings);
-router.patch("/", protect, authorize("admin"), updateKitchenSettings);
+router.get("/", protect, authorize("kitchen", "admin", "accountant"), requirePermission("kitchen"), getKitchenSettings);
+router.patch("/", protect, authorize("admin", "accountant"), requirePermission("kitchen"), updateKitchenSettings);
 
 export default router;
